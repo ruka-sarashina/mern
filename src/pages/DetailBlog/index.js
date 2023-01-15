37ae793631d433c954743ba7e69765191a071fce
taufik-fauzi <novops911@gmail.com>
2023-01-15 14:59:34 +0700

    style halaman detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { RegisterBg } from '../../assets'
+import './DetailBlog.scss';
 
 const DetailBlog = () => {
   return (
-    <div>
-      <p>Contet Detail Blog Page</p>
+    <div className='detail-blog-wrapper'>
+      <img className='img-cover' src={RegisterBg}  alt='thumb'/>
+      <p className='blog-title'>Title Blog</p>
+      <p className='blog-author'>Author - Date Post</p>
+      <p className='blog-body'>Content Blog</p>
     </div>
   )
 }
