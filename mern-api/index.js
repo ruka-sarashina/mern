7c1c973b53c3fd09e611a55a6065c9b8fe4627aa
taufik-fauzi <novops911@gmail.com>
2023-01-16 18:15:47 +0700

    membuat dummy endpoint res

--- mern-api/index.js
+++ mern-api/index.js
@@ -4,10 +4,12 @@ const bodyParser = require('body-parser');
 const app = express();
 const cors = require('cors');
 const productRoutes = require('./src/routes/products');
+const authRoutes = require('./src/routes/auth');
 
 app.use(bodyParser.json()) // type JSON
-app.use(cors())
+app.use(cors());
 
 app.use("/v1/customer", productRoutes);
+app.use('/v1/auth', authRoutes)
 
 app.listen(4000);
