662fbdb7eab6d0e9be0dd3729ed70b47dfb6383d
taufik-fauzi <novops911@gmail.com>
2023-01-16 08:17:48 +0700

    memahami basic router, req, dan respon

--- mern-api/index.js
+++ mern-api/index.js
@@ -1,15 +1,34 @@
+const { Router } = require('express');
 const express = require('express');
-const app = express();
 
-app.use(() => {
-    console.log("hello server");
-    console.log('hello satu dua');
-    console.log('hello satu tiga');
-    console.log('aaa');
-    console.log('aaa');
-    console.log('ab');
-    console.log('ab');
+const app = express();
+const router = express.Router();
+
+router.use('/products', (req, res, next) => {
+    // console.log('request: ', req);
+    // console.log('url:', req.originalUrl);
+    // console.log('method:', req.method);
+    res.json({name: "Taufik", email: "testing@gmail.com"})
+    next();
 })
+
+router.use('/price', (req, res, next) => {
+    res.json({price: 30000})
+    next();
+})
+
+router.get('/customers', (req, res, next) => {
+    res.json({title: "Customer"})
+    next();
+})
+
+// router.delete('/customers', (req, res, next) => {
+//     res.json({title: "Customer"})
+//     next();
+// })
+
+
+app.use('/', router)
 
 app.listen(4000);
 
