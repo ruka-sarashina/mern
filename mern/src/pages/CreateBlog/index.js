e45760abb8c8d6726faae6eb7248d5e757d079db
taufik-fauzi <novops911@gmail.com>
2023-01-19 15:18:14 +0700

    Refactor Menambah Posting Baru dan Menambahkan Redux

--- mern/src/pages/CreateBlog/index.js
+++ mern/src/pages/CreateBlog/index.js
@@ -2,37 +2,23 @@ import React, { useState } from "react";
 import { Input, Button, Upload, TextArea, Gap, Link } from "../../components";
 import "./CreateBlog.scss";
 import { useHistory } from "react-router-dom";
-import axios from "axios";
+import { useSelector, useDispatch } from "react-redux";
+import { setForm, setImgPreview } from "../../config/redux/action";
+import { postToAPI } from "../../config/redux/action/createBlogAction";
 
 const CreateBlog = () => {
-  const [title, setTitle] = useState("");
-  const [body, setBody] = useState("");
-  const [image, setImage] = useState("");
-  const [imagePreview, setImagePreview] = useState("");
+  const { form, imgPreview } = useSelector(state => state.createBlogReducer);
+  const { title, body } = form;
+  const dispatch = useDispatch();
 
   const onSubmit = () => {
-    console.log("title: ", title);
-    console.log("body: ", body);
-    console.log("image: ", image);
-
-    const data = new FormData();
-    data.append("title", title);
-    data.append("body", body);
-    data.append("image", image);
-
-    axios.post("http://localhost:4000/v1/blog/post", data, {
-        Headers: {
-          "content-type": "multipart/form-data"
-        }
-      })
-      .then(result => console.log("post succes", result))
-      .catch(err => console.log("err", err));
+    postToAPI(form);
   };
 
   const onImageUpload = e => {
     const file = e.target.files[0];
-    setImage(file);
-    setImagePreview(URL.createObjectURL(file));
+    dispatch(setForm("image", file));
+    dispatch(setImgPreview(URL.createObjectURL(file)));
   };
 
   const History = useHistory();
@@ -43,10 +29,13 @@ const CreateBlog = () => {
       <Input
         label="Post Title"
         value={title}
-        onChange={e => setTitle(e.target.value)}
+        onChange={e => dispatch(setForm("title", e.target.value))}
       />
-      <Upload onChange={e => onImageUpload(e)} img={imagePreview} />
-      <TextArea value={body} onChange={e => setBody(e.target.value)} />
+      <Upload onChange={e => onImageUpload(e)} img={imgPreview} />
+      <TextArea
+        value={body}
+        onChange={e => dispatch(setForm("body", e.target.value))}
+      />
       <Gap height={20} />
       <div className="button-action">
         <Button title="Save" onClick={onSubmit} />
